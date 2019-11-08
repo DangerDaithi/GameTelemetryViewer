aed560437ce02d6e2e3f1b4ba584b7be9d24a717
Fekshost <pope.david12@gmail.com>
2019-11-08 21:26:03 +0000

    Add prism and xunit dependencies. Skeleton project structure.

---
+++ GameTelemetryViewer.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameTelemetryViewer.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameTelemetryViewer.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f6cbf41-13d3-4574-a81d-5fa42c48aea5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GameTelemetryViewer.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+namespace GameTelemetryViewer.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Assert_True_IsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
